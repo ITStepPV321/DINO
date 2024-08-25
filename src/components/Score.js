f6f8116e1e96f5122cc8b78e255100b325c02f82
mrla3yt <gladandenys@gmail.com>
2024-08-26 01:17:10 +0300

    bug fixing

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,10 +1,9 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import { resetScore, updateRecord, updateScore } from './statsSlice';
-import { increaseDifficulty, resetDifficulty } from './configSlice';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { resetScore, updateRecord, updateScore } from "./statsSlice";
+import { increaseDifficulty, resetDifficulty } from "./configSlice";
 
-export default function Score()
-{
+export default function Score() {
     const stats = useSelector((state) => state.stats);
     const config = useSelector((state) => state.config);
 
@@ -15,62 +14,74 @@ export default function Score()
     useEffect(() => {
         let intervalId;
 
-        if (is_playing)
-        {
+        if (is_playing) {
             intervalId = setInterval(() => {
-                if ((stats.score) % config.distPerDiffIncrease === 0 && stats.score != 0)
-                {
-                    if (config.difficulty != config.maxDifficulty) {
+                if (stats.score % config.distPerDiffIncrease === 0 && stats.score !== 0) {
+                    if (config.difficulty !== config.maxDifficulty) {
                         dispatch(increaseDifficulty());
                     }
                 }
 
                 dispatch(updateScore());
 
-                if (stats.score == stats.record)
-                {
+                if (stats.score === stats.record) {
                     dispatch(updateRecord());
                 }
-            }, config.interval - (config.intervalPerDifficulty * config.difficulty));
+            }, config.interval - config.intervalPerDifficulty * config.difficulty);
         }
 
         return () => {
-            if (intervalId)
-            {
+            if (intervalId) {
                 clearInterval(intervalId);
             }
-        }
-    }, [dispatch, is_playing, stats.score, config.difficulty])
+        };
+    }, [
+        dispatch,
+        is_playing,
+        stats.score,
+        config.difficulty,
+        config.interval,
+        config.intervalPerDifficulty,
+        stats.record,
+        config.distPerDiffIncrease,
+        config.maxDifficulty,
+    ]);
 
     const playStopOnClick = () => {
         setIsPlaying(!is_playing);
         dispatch(resetScore());
         dispatch(resetDifficulty());
-    }
+    };
 
     const pauseOnClick = () => {
         setIsPlaying(!is_playing);
-    }
+    };
 
     return (
         <div className="speed">
-            <div className='score-container'>
-                <p className='score'>
-                    { stats.score }
-                </p>
-                <p className='record'>
-                    { stats.record }
-                </p>
+            <div className="score-container">
+                <p className="score">{stats.score}</p>
+                <p className="record">{stats.record}</p>
             </div>
 
-            <div className='btns-container'>
+            <div className="btns-container">
                 <button onClick={playStopOnClick}>
-                    <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/play--v1.png" alt="play--v1"/>
+                    <img
+                        width="30"
+                        height="30"
+                        src="https://img.icons8.com/ios-glyphs/30/play--v1.png"
+                        alt="play--v1"
+                    />
                 </button>
                 <button onClick={pauseOnClick}>
-                    <img width="30" height="30" src="https://img.icons8.com/ios-filled/50/pause--v1.png" alt="pause--v1"/>
+                    <img
+                        width="30"
+                        height="30"
+                        src="https://img.icons8.com/ios-filled/50/pause--v1.png"
+                        alt="pause--v1"
+                    />
                 </button>
             </div>
         </div>
-    )
+    );
 }
